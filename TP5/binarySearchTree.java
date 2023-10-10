dccbbb87ee9756657bd4bc324a951b0efc277216
FedeAlloza <allozafederico@gmail.com>
2023-10-10 08:50:19 -0300

    update 10.10 08:50

--- TP5/binarySearchTree.java
+++ TP5/binarySearchTree.java
@@ -58,6 +58,10 @@ public class binarySearchTree <AnyType extends Comparable<? super AnyType>> {
         root = insert(x, root);
     }
 
+    public void insertWithDuplicate(AnyType x){
+        root = insertWithDuplicate(x, root);
+    }
+
     public void remove(AnyType x){
         root = remove(x,root);
     }
@@ -126,9 +130,23 @@ public class binarySearchTree <AnyType extends Comparable<? super AnyType>> {
         } else if (x.compareTo(t.element) > 0) {
             t.right = insert(x, t.right);
         }
-        //else {
-        //    throw new DuplicateItemException(x.toString()); //Duplicate
-        //}
+        else {
+            throw new DuplicateItemException(x.toString()); //Duplicate
+        }
+        return t;
+    }
+
+    protected binaryNode<AnyType> insertWithDuplicate(AnyType x, binaryNode<AnyType> t){
+        if (t == null) {
+            t = new binaryNode<AnyType> (x);
+        } else if (x.compareTo(t.element) < 0) {
+            t.left = insertWithDuplicate(x, t.left);
+        } else if (x.compareTo(t.element) > 0) {
+            t.right = insertWithDuplicate(x, t.right);
+        }
+        /*else {
+            throw new DuplicateItemException(x.toString()); //Duplicate
+        }*/
         return t;
     }
 
@@ -146,9 +164,9 @@ public class binarySearchTree <AnyType extends Comparable<? super AnyType>> {
 
     protected binaryNode<AnyType> remove(AnyType x, binaryNode<AnyType> t){
         binaryNode<AnyType> temp;
-        if (t == null) {
+        //if (t == null) {
             throw new DuplicateItemException(x.toString());
-        }
+        //}
         if (x.compareTo(t.element) < 0) {
             t.left = remove(x, t.left);
         } else if (x.compareTo(t.element) > 0) {
--- TP5/tp5.java
+++ TP5/tp5.java
@@ -2,10 +2,12 @@ package INFO3.TP5;
 import java.util.Random;
 import java.util.Scanner;
 
+import INFO3.TP3.tp3;
+
 public class tp5 {
     public static void main(String[] args) {
         Scanner var = new Scanner(System.in);
-        int opcion, x;
+        int opcion, x, opcion3;
         char opcion2;
         binarySearchTree<Integer> arbolBinario = new binarySearchTree<>();
         do {
@@ -57,10 +59,25 @@ public class tp5 {
                 } while (opcion2 != 'z');
                     break;
                 case 3:
+                    System.out.println("EL ARBOL NO ACEPTA VALORES DUPLICADOS");
+                    System.out.println("ELIMINE LA EXCEPCION DEL DATO DUPLICADO E HICE UN NUEVO METODO");
+                    Random random2 = new Random();
+                    // Agregar 10 valores aleatorios al árbol
+                    for (int i = 0; i < 12; i++) {
+                        x = random2.nextInt(26);
+                        arbolBinario.insertWithDuplicate(x);
+                    }
                     break;
                 case 4:
+                    
                     break;
                 case 5:
+                    System.out.print("Ingrese el valor a borrar: ");
+                    x = var.nextInt();
+                    arbolBinario.remove(x);
+                    System.out.println("ARBOL BINARIO IN ORDER");
+                    // Imprimir el árbol en orden
+                    arbolBinario.printInOrder();
                     break;
             }       
         } while (opcion != 0);
